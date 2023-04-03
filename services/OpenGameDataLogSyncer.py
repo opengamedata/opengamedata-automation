d05f967c9bb1a5adfee3156b491b859e12c8788d
LswaN58 <lwswanson2@wisc.edu>
2023-04-03 11:47:45 -0500

    Make sure to check if mysql interface was non-null before attempting to use it!

--- services/OpenGameDataLogSyncer.py
+++ services/OpenGameDataLogSyncer.py
@@ -92,133 +92,136 @@ class OpenGameDataLogSyncer:
         Logger.Log("Begin syncing log entries for: " + str(dateToMigrate) + " from MySQL: " + mysqlTablePath + " to BigQuery: " + bqFqTableId)
 
         # Get the number of migrated & unmigrated source rows for the given date
-        migrationStatusCounts = self._mysqlInterface.GetMigrationStatusCountsByDate(dateToMigrate)
+        if self._mysqlInterface is not None:
+            migrationStatusCounts = self._mysqlInterface.GetMigrationStatusCountsByDate(dateToMigrate)
 
-        Logger.Log('For: ' + str(dateToMigrate) + ' Found ' + str(migrationStatusCounts[0]) + ' MySQL rows marked as requiring migration', logging.INFO)
-        Logger.Log('For: ' + str(dateToMigrate) + ' Found ' + str(migrationStatusCounts[1]) + ' MySQL rows marked as already migrated', logging.INFO)
+            Logger.Log('For: ' + str(dateToMigrate) + ' Found ' + str(migrationStatusCounts[0]) + ' MySQL rows marked as requiring migration', logging.INFO)
+            Logger.Log('For: ' + str(dateToMigrate) + ' Found ' + str(migrationStatusCounts[1]) + ' MySQL rows marked as already migrated', logging.INFO)
 
-        bqInterface = BigQueryInterface(self._config["BIGQUERY_CONFIG"])
+            bqInterface = BigQueryInterface(self._config["BIGQUERY_CONFIG"])
 
-        numBqTableEntriesBefore = 0
+            numBqTableEntriesBefore = 0
 
-        # If the desination table exists
-        if bqInterface.TableExists(bqFqTableId):
+            # If the desination table exists
+            if bqInterface.TableExists(bqFqTableId):
 
-            # Get a count of existing entries
-            numBqTableEntriesBefore = bqInterface.GetTableCount(bqFqTableId)
-            Logger.Log("For: " + str(dateToMigrate) + ' Found ' + str(numBqTableEntriesBefore) + ' existing BigQuery rows.', logging.INFO)
-        else:
-            # Create the table
-            bqInterface.CreateTable(bqFqTableId, BigQueryLogTableSchema.schema)
+                # Get a count of existing entries
+                numBqTableEntriesBefore = bqInterface.GetTableCount(bqFqTableId)
+                Logger.Log("For: " + str(dateToMigrate) + ' Found ' + str(numBqTableEntriesBefore) + ' existing BigQuery rows.', logging.INFO)
+            else:
+                # Create the table
+                bqInterface.CreateTable(bqFqTableId, BigQueryLogTableSchema.schema)
 
-        # Get a write interface instance
-        bqWriteInterface = BigQueryWriteInterface(self._config["BIGQUERY_CONFIG"], bqFqTableId)
+            # Get a write interface instance
+            bqWriteInterface = BigQueryWriteInterface(self._config["BIGQUERY_CONFIG"], bqFqTableId)
 
-        # Get a cursor for all source log entries on the given day
-        logEntriesCursor = self._mysqlInterface.GetLogEntriesByDate(dateToMigrate, SourceDataRowFormatType[self._config["MYSQL_CONFIG"]["SOURCE_TYPE"]])
+            # Get a cursor for all source log entries on the given day
+            logEntriesCursor = self._mysqlInterface.GetLogEntriesByDate(dateToMigrate, SourceDataRowFormatType[self._config["MYSQL_CONFIG"]["SOURCE_TYPE"]])
+                    
+            # Rows are dictionaries keyed by column name
+            mysqlRow = logEntriesCursor.fetchone()
+
+            numRequests = 0
+            numExportedRows = 0
+            numRowsInRequest = 0
+            maxRequestSizeInBytes = 10000000
+
+            # Explicitly set DEBUG log level to see the caught exceptions and debugging output from the Google libraries and API calls
+            if self._config["DEBUG_LEVEL"] == "DEBUG":
+                logging.basicConfig(level=logging.DEBUG)
+
+            estimatedRequestSize = 0
+            bqAppendRowsResponses = []
+
+            protoRows = types.ProtoRows()
+            bqAppendRowsRequest = None
+
+            # Offset of the first request should be zero
+            # Offset of subsequent requests should be equal to the number of rows we've previously sent
+            offset = 0
+
+            while mysqlRow is not None:
+
+                serializedRowData = BigQueryWriteInterface.AssembleSerializedRowData(mysqlRow, SourceDataRowFormatType[self._config["MYSQL_CONFIG"]["SOURCE_TYPE"]])
+                sizeOfserializedRowData = sys.getsizeof(serializedRowData)
                 
-        # Rows are dictionaries keyed by column name
-        mysqlRow = logEntriesCursor.fetchone()
+                # If adding this row to the request would push it over the max request limit of 10 MB
+                # we'll send the request and start a new request before adding the row
+                if sizeOfserializedRowData + estimatedRequestSize >= maxRequestSizeInBytes:
 
-        numRequests = 0
-        numExportedRows = 0
-        numRowsInRequest = 0
-        maxRequestSizeInBytes = 10000000
+                    Logger.Log("Estimated request size: " + str(estimatedRequestSize) + " bytes", logging.DEBUG)
+                    Logger.Log("Creating append rows request number: " + str(numRequests + 1) + " containing " + str(numRowsInRequest) + " rows with offset: " + str(offset) + " and sending", logging.INFO)
 
-        # Explicitly set DEBUG log level to see the caught exceptions and debugging output from the Google libraries and API calls
-        if self._config["DEBUG_LEVEL"] == "DEBUG":
-            logging.basicConfig(level=logging.DEBUG)
+                    # The size of a single AppendRowsRequest must be less than 10 MB in size
+                    # https://cloud.google.com/python/docs/reference/bigquerystorage/latest/google.cloud.bigquery_storage_v1.client.BigQueryWriteClient
+                    bqAppendRowsRequest = BigQueryWriteInterface.GetAppendRowsRequest(protoRows, offset)
 
-        estimatedRequestSize = 0
-        bqAppendRowsResponses = []
+                    # Send the request via the stream, store the repsonse in the array of responses
+                    bqAppendRowsResponses.append(bqWriteInterface.SendAppendRowsRequest(numRequests, bqAppendRowsRequest))
+                    
+                    # TODO: Could try deferring the response logging until all the requests have been sent. I believe the sends are supposed to be asynchronous, so .result() might wait until they resolve.
+                    # Not seeing much difference with days that don't have millions of log entries
+                    Logger.Log("For request number: " + str(numRequests + 1) + " with offset: " + str(offset) + " Request response result: " + str(bqAppendRowsResponses[len(bqAppendRowsResponses) - 1].result()), logging.DEBUG)
+                    
+                    # Update our offset
+                    offset += numRowsInRequest
+                    
+                    # Reset our request size and number of rows in the request, increase our offset
+                    estimatedRequestSize = 0
+                    numRowsInRequest = 0
 
-        protoRows = types.ProtoRows()
-        bqAppendRowsRequest = None
+                    numRequests += 1
 
-        # Offset of the first request should be zero
-        # Offset of subsequent requests should be equal to the number of rows we've previously sent
-        offset = 0
-
-        while mysqlRow is not None:
-
-            serializedRowData = BigQueryWriteInterface.AssembleSerializedRowData(mysqlRow, SourceDataRowFormatType[self._config["MYSQL_CONFIG"]["SOURCE_TYPE"]])
-            sizeOfserializedRowData = sys.getsizeof(serializedRowData)
             
-            # If adding this row to the request would push it over the max request limit of 10 MB
-            # we'll send the request and start a new request before adding the row
-            if sizeOfserializedRowData + estimatedRequestSize >= maxRequestSizeInBytes:
+                # If we're starting a new request
+                if numRowsInRequest == 0:
+                    # Create a batch of row data by appending proto2 serialized bytes to the serialized_rows repeated field.
+                    protoRows = types.ProtoRows()
 
+                # Add this row to the request
+                protoRows.serialized_rows.append(BigQueryWriteInterface.AssembleSerializedRowData(mysqlRow, SourceDataRowFormatType[self._config["MYSQL_CONFIG"]["SOURCE_TYPE"]]))
+                numRowsInRequest += 1
+                numExportedRows += 1
+                estimatedRequestSize += sizeOfserializedRowData
+
+                #Logger.Log(mysqlRow, logging.INFO)
+
+                # Get the next row
+                mysqlRow = self._mysqlInterface._db_cursor.fetchone()
+
+            self._mysqlInterface._db_cursor.close()
+
+            # If we have a request with rows that hasn't been sent yet
+            if not numRowsInRequest == 0:
+
+                # Send it now
                 Logger.Log("Estimated request size: " + str(estimatedRequestSize) + " bytes", logging.DEBUG)
-                Logger.Log("Creating append rows request number: " + str(numRequests + 1) + " containing " + str(numRowsInRequest) + " rows with offset: " + str(offset) + " and sending", logging.INFO)
-
-                # The size of a single AppendRowsRequest must be less than 10 MB in size
-                # https://cloud.google.com/python/docs/reference/bigquerystorage/latest/google.cloud.bigquery_storage_v1.client.BigQueryWriteClient
+                Logger.Log("Creating final append rows request number: " + str(numRequests + 1) + " containing " + str(numRowsInRequest) + " rows with offset: " + str(offset) + " and sending", logging.INFO)
                 bqAppendRowsRequest = BigQueryWriteInterface.GetAppendRowsRequest(protoRows, offset)
 
-                # Send the request via the stream, store the repsonse in the array of responses
                 bqAppendRowsResponses.append(bqWriteInterface.SendAppendRowsRequest(numRequests, bqAppendRowsRequest))
-                
-                # TODO: Could try deferring the response logging until all the requests have been sent. I believe the sends are supposed to be asynchronous, so .result() might wait until they resolve.
-                # Not seeing much difference with days that don't have millions of log entries
                 Logger.Log("For request number: " + str(numRequests + 1) + " with offset: " + str(offset) + " Request response result: " + str(bqAppendRowsResponses[len(bqAppendRowsResponses) - 1].result()), logging.DEBUG)
-                
-                # Update our offset
-                offset += numRowsInRequest
-                
-                # Reset our request size and number of rows in the request, increase our offset
-                estimatedRequestSize = 0
-                numRowsInRequest = 0
 
                 numRequests += 1
 
-        
-            # If we're starting a new request
-            if numRowsInRequest == 0:
-                # Create a batch of row data by appending proto2 serialized bytes to the serialized_rows repeated field.
-                protoRows = types.ProtoRows()
+            bqWriteInterface.CloseFinalizeAndCommit()
 
-            # Add this row to the request
-            protoRows.serialized_rows.append(BigQueryWriteInterface.AssembleSerializedRowData(mysqlRow, SourceDataRowFormatType[self._config["MYSQL_CONFIG"]["SOURCE_TYPE"]]))
-            numRowsInRequest += 1
-            numExportedRows += 1
-            estimatedRequestSize += sizeOfserializedRowData
+            Logger.Log(str(numExportedRows) + " MySQL log entries sent to: " + bqFqTableId, logging.INFO)
 
-            #Logger.Log(mysqlRow, logging.INFO)
+            numBqTableEntriesAfter = bqInterface.GetTableCount(bqFqTableId)
+            Logger.Log("For: " + str(dateToMigrate) + ' found ' + str(numBqTableEntriesAfter) + ' BigQuery rows', logging.INFO)
 
-            # Get the next row
-            mysqlRow = self._mysqlInterface._db_cursor.fetchone()
+            numRowsConfirmedInserted = numBqTableEntriesAfter - numBqTableEntriesBefore
 
-        self._mysqlInterface._db_cursor.close()
+            if numRowsConfirmedInserted < migrationStatusCounts[0]:
+                Logger.Log("Expected to migrate " + str(migrationStatusCounts[0]) + " rows from MySQL, but only " + str(numRowsConfirmedInserted) + " new rows found in BigQuery", logging.FATAL)
+                raise Exception("Missing expected log entries in BigQuery")
+                sys.exit(1) # This is unrecoverable, don't allow catching or continuing
 
-        # If we have a request with rows that hasn't been sent yet
-        if not numRowsInRequest == 0:
+            self._mysqlInterface.MarkLogEntriesAsSynced(dateToMigrate)        
+            Logger.Log("MySQL entries for " + str(dateToMigrate) + " have all been marked as synced")
 
-            # Send it now
-            Logger.Log("Estimated request size: " + str(estimatedRequestSize) + " bytes", logging.DEBUG)
-            Logger.Log("Creating final append rows request number: " + str(numRequests + 1) + " containing " + str(numRowsInRequest) + " rows with offset: " + str(offset) + " and sending", logging.INFO)
-            bqAppendRowsRequest = BigQueryWriteInterface.GetAppendRowsRequest(protoRows, offset)
-
-            bqAppendRowsResponses.append(bqWriteInterface.SendAppendRowsRequest(numRequests, bqAppendRowsRequest))
-            Logger.Log("For request number: " + str(numRequests + 1) + " with offset: " + str(offset) + " Request response result: " + str(bqAppendRowsResponses[len(bqAppendRowsResponses) - 1].result()), logging.DEBUG)
-
-            numRequests += 1
-
-        bqWriteInterface.CloseFinalizeAndCommit()
-
-        Logger.Log(str(numExportedRows) + " MySQL log entries sent to: " + bqFqTableId, logging.INFO)
-
-        numBqTableEntriesAfter = bqInterface.GetTableCount(bqFqTableId)
-        Logger.Log("For: " + str(dateToMigrate) + ' found ' + str(numBqTableEntriesAfter) + ' BigQuery rows', logging.INFO)
-
-        numRowsConfirmedInserted = numBqTableEntriesAfter - numBqTableEntriesBefore
-
-        if numRowsConfirmedInserted < migrationStatusCounts[0]:
-            Logger.Log("Expected to migrate " + str(migrationStatusCounts[0]) + " rows from MySQL, but only " + str(numRowsConfirmedInserted) + " new rows found in BigQuery", logging.FATAL)
-            raise Exception("Missing expected log entries in BigQuery")
-            sys.exit(1) # This is unrecoverable, don't allow catching or continuing
-
-        self._mysqlInterface.MarkLogEntriesAsSynced(dateToMigrate)        
-        Logger.Log("MySQL entries for " + str(dateToMigrate) + " have all been marked as synced")
-
-        
-        Logger.Log("Completed syncing log entries for: " + str(dateToMigrate))
+            
+            Logger.Log("Completed syncing log entries for: " + str(dateToMigrate))
+        else:
+            Logger.Log(f"Could not sync log entries for {str(dateToMigrate)}, the MySQLInterface was None!")
