9773dc29818a7cb0adef621ead690e0c19b91295
Luke Swanson <lwswanson2@wisc.edu>
2023-04-05 10:40:29 -0500

    Remove nargs, which weirdly turns arg value into a list.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from config.config import settings as script_settings
 parser = ArgumentParser(add_help=False)
 parser.add_argument("game", type=str.upper,
                     help="The game to use with the given command.")
-parser.add_argument("-m", "--max_days", nargs=1, type=int, required=False, default=100,
+parser.add_argument("-m", "--max_days", type=int, required=False, default=100,
                     help="Tell the program the maximum number of days to sync.")
 
 args : Namespace = parser.parse_args()
